1e24a6e63c5f1039468df637aaa724db089c0edb
Juro Oravec <juraj.oravec.josefson@gmail.com>
2019-11-30 13:55:04 +0100

    feat(org): add fr, sk and uk orgs [temp]

---
+++ org/fr/fr/middlewares.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+import pathlib
+
+from scrapy import signals
+from scrapy.downloadermiddlewares.defaultheaders import DefaultHeadersMiddleware
+from scrapy.utils.python import without_none_values
+
+from fr import exceptions
+
+
+class FrSpiderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the spider middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_input(self, response, spider):
+        # Called for each response that goes through the spider
+        # middleware and into the spider.
+
+        # Should return None or raise an exception.
+        return None
+
+    def process_spider_output(self, response, result, spider):
+        # Called with the results returned from the Spider, after
+        # it has processed the response.
+
+        # Must return an iterable of Request, dict or Item objects.
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a spider or process_spider_input() method
+        # (from other spider middleware) raises an exception.
+
+        # Should return either None or an iterable of Request, dict
+        # or Item objects.
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        # Called with the start requests of the spider, and works
+        # similarly to the process_spider_output() method, except
+        # that it doesn’t have a response associated.
+
+        # Must return only requests (not items).
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class FrDownloaderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        # Called for each request that goes through the downloader
+        # middleware.
+
+        # Must either:
+        # - return None: continue processing this request
+        # - or return a Response object
+        # - or return a Request object
+        # - or raise IgnoreRequest: process_exception() methods of
+        #   installed downloader middleware will be called
+        return None
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class SpiderExceptionMiddleware(object):
+
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+
+        def open_log_file(log_type):
+            filename = f'{spider.name}__{log_type}__{spider._time_tag}.log'
+            filepath = pathlib.Path(err_log_path).joinpath(filename)
+            mode = 'a' if filepath.exists() else 'w'
+            return filepath.open(mode, encoding='utf-8')
+
+        err_log_path = 'log'
+
+        if isinstance(exception, exceptions.SearchRequestException):
+            with open_log_file("search_errors") as f:
+                f.write(f"{response.url}\n")
+
+        elif isinstance(exception, exceptions.DetailRequestException):
+            with open_log_file("detail_errors") as f:
+                f.write(f"{response.request.body.decode('utf-8')}\n")
+
+
+class CustomHeadersMiddleware(DefaultHeadersMiddleware):
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        headers = without_none_values(
+            crawler.settings.get('CUSTOM_REQUEST_HEADERS'))
+        return cls(headers.items())
+
+    def process_request(self, request, spider):
+        request.headers.update(self._headers)
---
+++ org/sk/sk/spiders/sk_moj_spider.py
@@ -0,0 +1,338 @@
+
+from abc import ABC, abstractmethod
+import time
+from datetime import datetime
+from urllib import parse
+import re
+import sys
+
+import scrapy
+
+
+sys.path.insert(0, '../common')
+from common import util, components
+
+
+'''
+If there will ever be a need to scale, see http://blog.paracode.com/2017/10/29/building-market-analysis-products/
+'''
+
+
+class SKMoJRegistrySpider(
+    ABC,
+    components.LimitedResultsResolver
+    components.UrlModifier
+):
+    """
+    Scrapes entries from the Registries of Slovak Ministry of Justice
+    Data taken from http://www.orsr.sk/,
+    Run by the Ministry of Justice of the Slovak Republic
+    """
+
+    country = "Slovakia"
+    economic_area = "EEA"
+    liquidation_code = "v likvidácii"
+
+    max_entries_per_page = 500
+
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'sk.pipelines.CsvWriterPipeline': 400
+        }
+    }
+
+    @property
+    @abstractmethod
+    def name(self):
+        ...
+
+    def __init__(self, sources=None, sources_delim=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.sources_delim = sources_delim or " "
+        self.logger.debug(
+            f'Sources delimiter set to "{self.sources_delim}"')
+
+        self.sources = filter(None, (sources or "").split(self.sources_delim))
+        self.logger.debug('Sources: "{}"'.format('", "'.join(self.sources)))
+
+    def start_requests(self):
+        if self.sources:
+            for source in self.sources:
+                self.logger.debug(f'Opening source "{source}"')
+                with open(source, "r") as f:
+                    for line in f:
+                        for url in self.process_source_line(line):
+                            yield url
+        else:
+            for url in self.custom_start_requests():
+                yield url
+
+    def parse(self, response):
+        # entry url
+        if self.is_entry(response.url):
+            yield next(self.parse_entry(response))
+        # search url
+        else:
+            for url_req in self.parse_search(response):
+                yield url_req
+
+    def parse_search(self, response):
+        entries_num_selector = '//body/table[2]//td[2]/b[2]/text()'
+        entry_selector = f'//body/table[3]//td[position()=3]//div[{self.xpath_class(["bmk"])} and position()=1]/a[1]/@href'
+
+        def get_page_entries_info(response):
+            entries_info = response.xpath(entries_num_selector).get()
+            start, end, total = map(
+                int,
+                entries_info.replace("/", "-").split("-")
+            )
+            return start, end, total
+
+        def is_last_page(displayed, total):
+            return displayed >= total
+
+        def get_next_page_url(response):
+            url_obj, q_obj = self.unpack_url(response)
+            q_obj["STR"] = int(q_obj.get("STR", 1)) + 1
+            return self.pack_url(url_obj, q_obj)
+
+        # follow links to entry pages
+        entries = response.xpath(entry_selector).getall()
+        for href in entries:
+            yield response.follow(href, self.parse_entry)
+
+        # follow pagination links
+        _, displayed, total = get_page_entries_info(response)
+
+        if is_result_limited(total):
+            new_urls = self.resolve_result_limit(response)
+
+        elif not is_last_page(displayed, total):
+            new_urls = [get_next_page_url(response)]
+        else:
+            new_urls = []
+
+        for new_url in new_urls:
+            yield response.follow(new_url, self.parse_search)
+
+        # self.logger.debug(f"entries_found = {len(entries)}")
+        # self.logger.debug(f"is_last_page = {is_last_page(response)}")
+
+    def parse_entry(self, response):
+
+        def city_from_address(a):
+            def is_postcode(s):
+                try:
+                    int(s.replace(" ", ""))
+                    return True
+                except ValueError:
+                    return False
+
+            a_parts = a.split("\r")
+
+            if len(a_parts) == 1:
+                city = a if is_postcode(a) else ""
+            else:
+                # Last line can be postcode or city
+                city = a_parts[-2] if is_postcode(a_parts[-1]) else a_parts[-1]
+
+            return city.split("-")[0].strip()
+
+        meta_field_parent_selector = f'//body/table[3]//span[{self.xpath_class(["tl"])}]/parent::td'
+        meta_field_title_selector = f'/descendant::span[{self.xpath_class(["tl"])}]/text()'
+        meta_field_value_selector = f'/descendant::span[{self.xpath_class(["ra", "ro"])}]/text()'
+
+        field_parent_selector = f'//body/table[position()>3]//span[{self.xpath_class(["tl"])}]/ancestor::tr'
+        field_title_selector = f'/descendant::td[position()=1]/span[{self.xpath_class(["tl"])}]/text()'
+        field_value_selector = f'/descendant::td[position()=2]//td[position()=1]//span[{self.xpath_class(["ra", "ro"])}]/text()'
+
+        meta_fields, fields = map(
+            self.parse_entry_fields,
+            [response] * 2,
+            [meta_field_parent_selector, field_parent_selector],
+            [meta_field_title_selector, field_title_selector],
+            [meta_field_value_selector, field_value_selector]
+        )
+
+        names = [fields[k] for k in fields if "obchodné meno" in k]
+        liquidation = (
+            [self.is_in_liquidation(name) for name in names] or [False]
+        )[0]
+        addresses = [fields[k] for k in fields if "sídlo" in k]
+        city = (
+            [city_from_address(a) for a in addresses] or [""]
+        )[0]
+
+        data = {
+            **meta_fields,
+            **fields,
+            'city': city,
+            'country': self.country,
+            'economic_area': self.economic_area,
+            'registry': response.url,
+            'liquidation': liquidation,
+            'timestamp': int(time.time() * 1000)
+        }
+
+        if "ičo" in data:
+            data["ičo"] = "".join(data["ičo"].split())
+        if "deň zápisu" in data:
+            data["deň zápisu (timestamp)"] = int(datetime.strptime(
+                "".join(data["deň zápisu"].split()),
+                "%d.%m.%Y"
+            ).timestamp() * 1000)
+        if "deň výmazu" in data:
+            data["deň výmazu (timestamp)"] = int(datetime.strptime(
+                "".join(data["deň výmazu"].split()),
+                "%d.%m.%Y"
+            ).timestamp() * 1000)
+
+        yield data
+
+    def parse_entry_fields(self, response, parent_selector, title_selector, value_selector):
+
+        def get_title(el, selector):
+            title = el.xpath(selector).get("").replace(":", "").strip().lower()
+            # self.logger.debug(f"\nselector: {selector}\ntitle: {title}\n")
+            return title
+
+        def get_value(el, selector):
+            value = self.multiline(self.unquote(
+                "\n".join(
+                    map(str.strip, el.xpath(value_selector).getall())
+                )
+            ))
+            # self.logger.debug(f"\nselector: {selector}\nvalue: {value}\n")
+            return value
+
+        parents = [
+            scrapy.http.TextResponse("", body=el.get(), encoding="utf-8")
+            for el in response.xpath(parent_selector)
+        ]
+
+        data = {
+            get_title(el, title_selector): get_value(el, value_selector)
+            for el in parents
+        }
+
+        return data
+
+    def process_source_line(self, line):
+        yield scrapy.Request(line.strip())
+
+    def is_entry(self, url):
+        return "orsr.sk/vypis.asp" in url
+
+    def is_in_liquidation(self, name):
+        return self.liquidation_code in name
+
+    def is_result_limited(self, total):
+        return total > self.max_entries_per_page
+
+    def unquote(self, s):
+        while len(s) and s[0] == s[-1] and all(c in '\'"' for c in [s[0], s[-1]]):
+            s = s[1:-1]
+        return s
+
+    def multiline(self, s):
+        return s.replace('\n', '\r')
+
+    def xpath_class(self, classes):
+        return " or ".join(
+            f"contains(concat(' ', normalize-space(@class),' '),' {cls} ')"
+            for cls in classes
+        )
+
+
+class SKMoJBusinessesByIdSpider(SKMoJRegistrySpider, scrapy.Spider):
+    """
+    Scrapes entries from the Business Registry of Slovak Ministry of Justice
+    Data taken from http://www.orsr.sk/,
+    Run by the Ministry of Justice of the Slovak Republic
+    """
+
+    name = "businesses_by_id"
+
+    id_digit_len = 8
+    start = 0
+    end = -1
+
+    def custom_start_requests(self):
+        count = self.start
+        if end < 0:
+            max = pow(10, self.id_digit_len)
+        else:
+            max = end
+
+        while count < max:
+            id = str(count).zfill(self.id_digit_len)
+            yield scrapy.Request(f"http://www.orsr.sk/hladaj_ico.asp?ICO={id}&SID=0")
+            count += 1
+
+    def resolve_result_limit(self, response):
+        url_obj, q_obj = self.unpack_url(response)
+        # Court Codes:
+        # 1 - Any
+        # 2 - Bratislava I
+        # 3 - Banska Bystrica
+        # 4 - Kosice I
+        # 5 - Zilina
+        # 6 - Trencin
+        # 7 - Trnava
+        # 8 - Presov
+        # 9 - Nitra
+        for sid in range(2, 10):
+            q = q_obj.copy()
+            q["SID"] = sid
+            yield self.pack_url(url_obj, q)
+
+
+class SKMoJBusinessesByNameSpider(
+    SKMoJRegistrySpider,
+    components.LimitedResultsByQueryResolver,
+    scrapy.Spider
+):
+    """
+    Scrapes entries from the Business Registry of Slovak Ministry of Justice
+    Data taken from http://www.orsr.sk/,
+    Run by the Ministry of Justice of the Slovak Republic
+    """
+
+    name = "businesses_by_name"
+
+    sources_delim = " "
+    query_param = 'OBMENO'
+
+    def __init__(self, sources=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.sources = (sources or "").split()
+        self.string_combinations.variants = {'c': ['ch']}
+
+    def process_source_line(self, line):
+        if self.is_entry(line):
+            return scrapy.Request(line.strip())
+
+        url_obj, q_obj = self.unpack_url(url=line.strip())
+        name = q_obj.get(self.query_param, "")
+        page = int(q_obj.get("STR", 1))
+        i = len(name) - 1
+
+        for url in self.search(name=name, page=page):
+            yield url
+
+    def custom_start_requests(self):
+        for url in self.search():
+            yield url
+
+    def search(self, name="", page=1):
+        names = self.string_combinations.generate(seed=name)
+        for name in names:
+            # OBMENO = Business Name (Obchodne meno)
+            # PF = Legal form ID (Pravna forma)
+            # SID = Court ID (Sud)
+            # R = "on" == active entries only
+            # S = "on"== search anywhere in name; "off" == entry starts with query
+            p = f"&STR={page}" if page > 1 else ""
+            yield scrapy.Request(f"http://www.orsr.sk/hladaj_subjekt.asp?OBMENO={name}&PF=0&SID=0&S=off&R=on{p}")
